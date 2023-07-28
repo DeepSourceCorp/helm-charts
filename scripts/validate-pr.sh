312efe67e2dfe0e6ddcef8a7e2f8177313254b15
Rohan Kumar <rohan.kumar@deepsource.io>
2023-07-28 19:10:56 +0530

    chore: init repo with github workflows and scripts

---
+++ scripts/validate-pr.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+main() {
+    local repo_root
+    repo_root=$(git rev-parse --show-toplevel)
+
+    local changed
+    changed=$(ct list-changed --config "$repo_root/ct.yaml")
+
+    if [[ -z "$changed" ]]; then
+        exit 0
+    fi
+
+    local num_changed
+    num_changed=$(wc -l <<< "$changed")
+
+    if ((num_changed > 1)); then
+        echo "This PR has changes to multiple charts. Please create individual PRs per chart." >&2
+        exit 1
+    fi
+}
+
+main
